ade8e7f0c804fee43475e90b718bcf220a83af59
CryptoFaust <97149486+CryptoFaust@users.noreply.github.com>
2022-06-27 05:23:03 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мой сайт';
+$name = 'Faust';
+$image = 'img/unknown-raccoon.svg';
+$email = 'H.Faust.PHD@gmail.com';
+$phone = '+70000000000';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
